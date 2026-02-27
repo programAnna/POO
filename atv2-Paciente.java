f26bfc666e32125ab93cf9ff566da509679a85a4
Anna Louro <annab.louro@gmail.com>
2026-02-27 18:18:57 -0300

    Create atv2-Paciente.java

---
+++ atv2-Paciente.java
@@ -0,0 +1,16 @@
+public class Paciente
+{
+    public String nome;
+    public String cpf;
+    public String telefone;
+    public String genero;
+    public Int idade;
+    
+    public void cadastrar(){
+        
+    }
+    
+    public void consultar(){
+        
+    }
+}
