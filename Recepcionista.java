9dc4d23d02e13f6e651188aee9e1a531ff1ad102
Anna Louro <annab.louro@gmail.com>
2026-02-27 17:59:15 -0300

    Create Recepcionista.java

---
+++ Recepcionista.java
@@ -0,0 +1,10 @@
+public class Recepcionista{
+    public String nome;
+    public String cpf;
+    public String telefone;
+    public String senha;
+    
+    public void acessar(){
+        
+    }
+}
