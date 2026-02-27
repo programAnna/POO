7ca088dc194fda513240c8fcd109e6ce5761f8da
Anna Louro <annab.louro@gmail.com>
2026-02-27 18:08:42 -0300

    Create Receita.java

---
+++ Receita.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate; 
+
+public class Receita{
+    public String consulta;
+    public LocalDate data;
+    public String historico;
+    
+    public void preescrever(){
+        
+    }
+
+    public void consultar(){
+        
+    } 
+    
+}
