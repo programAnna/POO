d6b69be0d29c3f6db07424b864960432c4afe452
Anna Louro <annab.louro@gmail.com>
2026-02-27 18:15:37 -0300

    Create Agenda.java

---
+++ Agenda.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate; 
+import java.time.LocalTime;
+
+public class Agenda{
+    public LocalDate data;
+    public LocalTime hora;
+    public String medico;
+    public String paciente;
+    
+    public void consultar(){
+        
+    }
+}
