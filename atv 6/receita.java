44c1982f9900b74dc7f1c9f86f54e73d497064b6
Anna Louro <annab.louro@gmail.com>
2026-03-29 23:31:04 -0300

    Create receita.java

---
+++ atv 6/receita.java
@@ -0,0 +1,21 @@
+public class Receita extends  Procedimento {
+   
+    public Receita(String data, String descritivo) {
+        this.data = data;
+        this.descritivo = descritivo;
+    }
+
+    public void preescrever(){};
+    public void consultar(){}
+   
+    public Receita() {
+    }
+
+   
+    public void mostrar() {
+       
+    };
+
+
+    
+}
