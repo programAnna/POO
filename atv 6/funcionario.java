25bd5928dbe1310e737b92e0cf2e413dea563792
Anna Louro <annab.louro@gmail.com>
2026-03-29 23:28:53 -0300

    Create funcionario.java

---
+++ atv 6/funcionario.java
@@ -0,0 +1,34 @@
+public abstract class Funcionario {
+
+       String telefone;
+    String senha;
+     String nome;
+
+     
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
