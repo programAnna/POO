2973939b4e86f27733b7ccd79002ffe523efcc5f
Anna Louro <annab.louro@gmail.com>
2026-04-09 23:40:55 -0300

    Create Medico.java

---
+++ atv 2/Medico.java
@@ -0,0 +1,10 @@
+public class Medico{
+    public String nome;
+    public String crm;
+    public String especialidade;
+    public String senha;
+    
+    public void acessar(){
+        
+    }
+}
