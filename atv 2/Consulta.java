c41eaa9822c2df8127050148a064ee9c1e36ae95
Anna Louro <annab.louro@gmail.com>
2026-04-09 23:40:06 -0300

    Create Consulta.java

---
+++ atv 2/Consulta.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate; 
+import java.time.LocalTime;
+
+public class Consulta{
+    public LocalDate data;
+    public LocalTime hora;
+    public String medico;
+    public String paciente;
+    public String motivo;
+    public String historico;
+    
+    public void marcar(){
+        
+    }
+    
+    public void cancelar(){
+        
+    }
+ 
+    public void consultar(){
+        
+    } 
+    
+    public void realizar(){
+        
+    }
+
+    public void atualizar(){
+        
+    }
+    
+}
