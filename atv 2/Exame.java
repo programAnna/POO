0595e77f684596edda4fee14f48196bb8423dd19
Anna Louro <annab.louro@gmail.com>
2026-04-09 23:40:33 -0300

    Create Exame.java

---
+++ atv 2/Exame.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate; 
+
+public class Exame{
+    public String consulta;
+    public LocalDate data;
+    public String historico;
+    
+    public void solicitar(){
+        
+    }
+
+    public void consultar(){
+        
+    } 
+    
+}
