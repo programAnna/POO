88fcbc8bf64fb0153111131189c902c6c8b91ea4
Anna Louro <annab.louro@gmail.com>
2026-05-20 23:09:46 -0300

    Add files via upload

---
+++ atv9/src/main/java/com/example/bean/Medico.java
@@ -0,0 +1,50 @@
+package com.example.bean;
+
+public class Medico {
+
+    private int codigo;
+    private String nome;
+    private String especialidade;
+    private String crm;
+
+    public Medico() {
+    }
+
+    public Medico(String nome, String especialidade, String crm) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.crm = crm;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+}
